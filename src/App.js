c7f250fe9ba32f21968fe395d34d399a348c58cc
gabygarro <ggarroab@gmail.com>
2023-06-30 21:48:39 -0600

    MVP

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./gradient-animation.css"
+import "./App.css"
+import { Player } from './Player'
+
+function App() {
+
+  return <div className="app">
+    <Player />
+  </div>
+}
+
+export default App
---
+++ src/Player.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState, useRef } from 'react'
+import Button from '@mui/material/Button'
+
+const musicStreamUrl = "https://stream-160.zeno.fm/0r0xa792kwzuv"
+const sjoAtcUrl = "https://s1-fmt2.liveatc.net/mroc?nocache=2023061720454128791"
+
+export const Player = () => {
+  const [playing, setPlaying] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [canPlayMusic, setCanPlayMusic] = useState(false)
+  const [canPlayAtc, setCanPlayAtc] = useState(false)
+  const musicAudio = useRef()
+  const atcAudio = useRef()
+
+  useEffect(() => {
+    musicAudio.current = new Audio(musicStreamUrl)
+    musicAudio.current.addEventListener('canplay', () => {
+      setCanPlayMusic(true)
+    })
+    atcAudio.current = new Audio(sjoAtcUrl)
+    atcAudio.current.addEventListener('canplay', () => {
+      setCanPlayAtc(true)
+    })
+  }, [])
+
+  useEffect(() => {
+    if (loading && canPlayMusic && canPlayAtc) {
+      musicAudio.current.play()
+      atcAudio.current.play()
+      setPlaying(true)
+      setLoading(false)
+    }
+  }, [canPlayMusic, canPlayAtc])
+
+  const playOnClick = () => {
+    setLoading(true)
+    if (!canPlayMusic || !canPlayAtc) return
+    if (!playing) {
+      musicAudio.current.play()
+      atcAudio.current.play()
+      setPlaying(true)
+      setLoading(false)
+    } else if (playing) {
+      musicAudio.current.stop()
+      atcAudio.current.stop()
+      setPlaying(false)
+      setLoading(false)
+    }
+  }
+
+  return (<>
+  <h1 className="title">loficocotower</h1>
+  <div className="content">
+    <Button
+      className="playButton"
+      onClick={playOnClick}
+      disabled={loading}>
+        {loading ? 'loading...' : (playing ? 'stop' : 'play')}
+    </Button>
+  </div></>)
+}
