4497bfef500a8094dca6a123a6baba04c566076e
gabygarro <ggarroab@gmail.com>
2023-11-20 19:57:16 -0600

    Songs

---
+++ src/music.js
@@ -0,0 +1,138 @@
+export const songs = [
+  {
+    name: "Floater - Auxjack",
+    youtube: "https://youtu.be/uJv3YCk9wD4?si=jq5Xdtd2EObO32_T",
+  },
+  {
+    name: "Jazzy Wave - yourneighborsclassicbeats",
+    youtube: "https://youtu.be/P-zJCLBqZc4?si=cr-z9PQMcVtafWqy",
+  },
+  {
+    name: "Controlla - Idealism",
+    youtube: "https://youtu.be/_mAPAtjWZGE?si=NbOg2kTr9UT582GM",
+  },
+  {
+    name: "Started Ballin' - Brenk Sinatra",
+    youtube: "https://youtu.be/JbXkjRqWTxc?si=6R10eJ6BLqrzg_9G",
+  },
+  {
+    name: "Wonderful Day - Kind Puppy",
+    youtube: "https://youtu.be/72b8D6s_y_U?si=0Wfrohn1N8U1w_Vg",
+  },
+  {
+    name: "Santa Anna - Tom Doolie",
+    youtube: "https://youtu.be/mHKNroJOcig?si=BC8twV5gaJQlgFZd",
+  },
+  {
+    name: "Far Away - sleepsleepsleep",
+    youtube: "https://youtu.be/0C1NFu4NB_4?si=xPnLXfx5DaNXmUMe",
+  },
+  {
+    name: "Bees And Apple Trees · Heinrich Schlupf",
+    youtube: "https://youtu.be/EnLWwwtO1-A?si=xcesYnrX-DK8CO_f",
+  },
+  {
+    name: "Bustlin - LESKY",
+    youtube: "https://youtu.be/cafxLTAWi-g?si=3RywWhW6CnV9ZI9S",
+  },
+  {
+    name: "The Afternoon - Boztown",
+    youtube: "https://youtu.be/uyemSV1SpnE?si=LQrwipHZHGLNctsr",
+  },
+  {
+    name: "Mramor - Chau Sara",
+    youtube: "https://youtu.be/ogc58xMpPCc?si=ugftYdoNKhqFo4GD",
+  },
+  {
+    name: "Nonchalant (feat. Hiroshi Suzuki) - Dire Beats",
+    youtube: "https://youtu.be/tsw1LgVkFjE?si=3WvMWW3FbLgr9Ss4",
+  },
+  {
+    name: "i'm closing my eyes - potsu",
+    youtube: "https://youtu.be/xSB8trUFX1A?si=NKVVvdGVnBwlOptN",
+  },
+  {
+    name: "Sonria - Chris Mazuera",
+    youtube: "https://youtu.be/u04paoFoJeo?si=Lc91SZvlgzyLwVM5",
+  },
+  {
+    name: "Affection - Jinsang",
+    youtube: "https://youtu.be/LbHsWjX9dv4?si=ONMIou8AhY-9mgEq",
+  },
+  {
+    name: "grainy days - Moody.",
+    youtube: "https://youtu.be/L2apFB6EF-Y?si=SKXgiI7ydI5xyxNi",
+  },
+  {
+    name: "Aftermath - Beamer",
+    youtube: "https://youtu.be/uCW2sZZofhk?si=szEDvhAUfAEx23F9",
+  },
+  {
+    name: "可可 (feat. 卫彬月) - DouDou",
+    youtube: "https://youtu.be/XZ9DKiNZszM?si=2-hmV8VYMMvFfe53",
+  },
+  {
+    name: "Summer Soft - Shuko",
+    youtube: "https://youtu.be/J1AOKFRqCjg?si=GOtkSJzRG0xHHSLH",
+  },
+  {
+    name: "thinking of you w/ Kyle McEvoy - mommy x Philanthrope",
+    youtube: "https://youtu.be/XNd6d6H1LBo?si=KitvYmUqDiz0AqtD",
+  },
+  {
+    name: "Moony - The BREED",
+    youtube: "https://youtu.be/SSy4VdjEJKM?si=_jwi_HsYSRFx1ms8",
+  },
+  {
+    name: "Lost In This Moment - Evil Needle",
+    youtube: "https://youtu.be/0am_BfODDvc?si=vxhQL_IiVR0eYU7d",
+  },
+  {
+    name: "she - Sans",
+    youtube: "https://youtu.be/xv3qKrCq2w8?si=GdXKREJNXD69GVDy",
+  },
+  {
+    name: "Traveller - Dualizm",
+    youtube: "https://youtu.be/pZC82a-2eo0?si=plmFAPIF2FDwR7aM",
+  },
+  {
+    name: "Gardenia - DRWN",
+    youtube: "https://youtu.be/Ypu8170x5Vw?si=3ykxMKwA0FlOFxTA",
+  },
+  {
+    name: "Azimuth - Dpsht",
+    youtube: "https://youtu.be/Jcp48M-vRJw?si=Kt0eJEFDjlXE8zNq",
+  },
+  {
+    name: "Opiate - Evil Needle",
+    youtube: "https://youtu.be/YeSgtLJQGJs?si=-_sqpMqJ45fcxmr9",
+  },
+  {
+    name: "Future - Paul Grant",
+    youtube: "https://youtu.be/_LY6aFUVmOU?si=FvLMFzGfLLbWN4uB",
+  },
+  {
+    name: "Rainbow Roll - Strehlow",
+    youtube: "https://youtu.be/SCTMdJ1cuoU?si=GFHjWIqg76pyCDtC",
+  },
+  {
+    name: "1. Dreams - briqs",
+    youtube: "https://youtu.be/TEOwoK-CMes?si=8bjAglroUXFQTitI",
+  },
+  {
+    name: "Still - Idealism",
+    youtube: "https://youtu.be/1FxeCa6kypU?si=yBxdgccWw1YieCFx",
+  },
+  {
+    name: "A Day at the Beach - Pandrezz",
+    youtube: "https://youtu.be/UiCRuNo4dGg?si=B0hvoJyyyAOGbxO1",
+  },
+  {
+    name: "Eternal Youth - RŮDE",
+    youtube: "https://youtu.be/3uXHaVENo6E?si=xaSKC8NMyDh8S0X5",
+  },
+  {
+    name: "Birds Fly Higher Than The Moon - Yasper",
+    youtube: "https://youtu.be/EX2d37tT5fo?si=FDKcLL-s18PZ0jLR",
+  },
+]
