975ee1d23f37c742ba611ecefc17c5bdfc596563
gabygarro <ggarroab@gmail.com>
2026-09-02 17:34:52 -0600

    Replace plausible instance

--- pages/+bodyHtmlEnd.ts
+++ pages/+bodyHtmlEnd.ts
@@ -5,7 +5,7 @@ export default (pageContext: PageContext) => {
   if (isProduction) {
     return `
       <!-- Privacy-friendly analytics by Plausible -->
-      <script async src="https://plausible.io/js/pa-njte1QHn-wQqETGbA9IaL.js"></script>
+      <script async src="https://analytics.gabygarro.dev/js/pa-HmDnhSpJ1Yws36Bwewy9d.js"></script>
       <script>
         window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
         plausible.init()
